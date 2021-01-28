a78efa676ddd03dfeff347b99fa889b1a3bf74d6
Kaitlyn Hair <46422034+kaitlynhair@users.noreply.github.com>
2021-01-28 18:22:53 +0000

    Update example_usage.R

--- examples/example_usage.R
+++ examples/example_usage.R
@@ -1,10 +1,45 @@
+#----- Set up and load packages ------
+# install.packages("githubinstall")
+# library(githubinstall)
+#install_github("camaradesuk/RegexRoB")
+
+library(RegexRoB)
 library(readr)
 
-papers <- read.csv("data/papers_to_assess.csv")#
-pdftotext <- "S:/Regex_Dev/xpdfbin-win-3.04/bin64/pdftotext.exe"
+#-------- Read in df of articles ------
+papers <- read.csv("data/papers_to_assess.csv")
 
+#-------- Simple RoB extraction ------
+
+# extract ROB reporting 
+# define linksearchHeader - column where PDF path is found
 RoB_assessment <- RiskOfBiasIdentification(
   searchingData = papers,
   linkSearchHeaders = "PdfRelativePath",
-  ignoreCase = TRUE ,
-  conversionSoftware = pathtotext)
+  ignoreCase = TRUE)
+
+
+#-------- RoB extraction with added functions to remove Introduction/Background and Reference sections of PDFs (experimental feature) ------
+
+# extract ROB reporting 
+# define linksearchHeader - column where PDF path is found
+RoB_assessment <- RiskOfBiasIdentification(
+  searchingData = papers,
+  linkSearchHeaders = "PdfRelativePath",
+  cutRefs = TRUE,
+  cutIntro = TRUE,
+  ignoreCase = TRUE)
+
+
+#-------- RoB extraction with custom regex dictionary ------
+
+# extract ROB reporting 
+# define linksearchHeader - column where PDF path is found
+RoB_assessment <- CountTermsInStudies(
+  dictionary = "pathtocustomdictionary/regex.csv" 
+  searchingData = papers,
+  linkSearchHeaders = "PdfRelativePath",
+  ignoreCase = TRUE)
+
+
+
